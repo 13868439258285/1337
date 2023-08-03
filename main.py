820a25bec33ab90de0a6a5f6c27271b83f8c5f08
13868439258285 <141268629+13868439258285@users.noreply.github.com>
2023-08-03 02:42:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -136,4 +136,4 @@ def tiktok():
     return render_template('tiktok.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=1337)
