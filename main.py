abdf2c32d973a34b20032c6e8ada3593f272a8ff
13868439258285 <141268629+13868439258285@users.noreply.github.com>
2023-08-04 12:08:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+
 from flask import Flask, render_template, request, jsonify
 from concurrent.futures import ThreadPoolExecutor
 import requests
@@ -6,18 +9,17 @@ import random
 import time
 import threading
 from requests.exceptions import ProxyError, SSLError
+
 app = Flask(__name__)
 
-
-# Lock for thread-safe access to cooldowns file
+# Lock for thread-safe access to blacklist and cooldowns files
+blacklist_lock = threading.Lock()
 cooldowns_lock = threading.Lock()
 
 def get_proxy():
     """Returns a random proxy from the list of proxies."""
     with open("proxies.txt", "r") as f:
         proxies = f.readlines()
-        print("successfully loaded proxies")
-        time.sleep(1)
     proxy = random.choice(proxies).strip()
     return proxy
 
@@ -26,10 +28,7 @@ def get_user_agent():
     with open("useragents.txt", "r") as f:
         user_agents = f.readlines()
     user_agent = random.choice(user_agents).strip()
-    print("successfully loaded useragents")
-    time.sleep(1)
     return user_agent
-                    
 
 def open_product_page(url, proxy, max_retry=3):
     """Opens the product page with the given URL and proxy."""
@@ -55,7 +54,6 @@ def open_product_page(url, proxy, max_retry=3):
     print("Max retries reached. Failed to open product page with any proxy.")
     return False
 
-
 def simulate_views(url, views, use_delay, delay):
     proxies = [get_proxy() for _ in range(views)]
     success_count = 0
@@ -70,6 +68,35 @@ def simulate_views(url, views, use_delay, delay):
 
     return success_count
 
+def check_blacklist(user_ip):
+    """Check if the user's IP is blacklisted."""
+    with blacklist_lock:
+        with open("blacklist.txt", "r") as f:
+            for line in f:
+                if line.strip() == user_ip:
+                    return True
+    return False
+
+def add_to_blacklist(user_ip):
+    """Add the user's IP to the blacklist for 30 minutes."""
+    with blacklist_lock:
+        with open("blacklist.txt", "a") as f:
+            f.write(user_ip + "\n")
+
+    # Schedule the removal of the IP from the blacklist after 30 minutes
+    threading.Timer(1800, remove_from_blacklist, args=[user_ip]).start()
+
+def remove_from_blacklist(user_ip):
+    """Remove the user's IP from the blacklist."""
+    with blacklist_lock:
+        with open("blacklist.txt", "r") as f:
+            lines = f.readlines()
+
+        with open("blacklist.txt", "w") as f:
+            for line in lines:
+                if line.strip() != user_ip:
+                    f.write(line)
+
 def check_cooldown(user_ip):
     """Check if the user is still in cooldown period."""
     with cooldowns_lock:
@@ -82,7 +109,7 @@ def check_cooldown(user_ip):
                 cooldowns[ip] = float(timestamp)
 
         last_used_time = cooldowns.get(user_ip, 0)
-        cooldown_time = 10 * 60  # 10 minutes cooldown
+        cooldown_time = 600  # 10 minutes cooldown
 
         if current_time - last_used_time < cooldown_time:
             return True
@@ -96,36 +123,51 @@ def check_cooldown(user_ip):
 
         return False
 
+def check_whitelist(user_ip):
+    """Check if the user's IP is whitelisted."""
+    with open("whitelist.txt", "r") as f:
+        for line in f:
+            if line.strip() == user_ip:
+                return True
+    return False
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         user_ip = request.remote_addr
 
-        if check_cooldown(user_ip):
+        if check_whitelist(user_ip):
+            # If the user is whitelisted, skip cooldown and blacklist checks
+            url = request.form.get('url')
+            views = int(request.form.get('views'))
+            use_delay = 'use_delay' in request.form
+            delay = int(request.form.get('delay')) if use_delay else 0
+
+            print(f"Botting URL: {url} with {views} views.")
+            print(f"Use Delay: {use_delay}, Delay: {delay}")
+
+            success_count = simulate_views(url, views, use_delay, delay)
+            return jsonify({'status': 'success', 'views': success_count})
+
+        elif check_blacklist(user_ip):
+            # If the user is blacklisted, return an error
+            return jsonify({'status': 'error', 'message': 'You are blacklisted.'})
+
+        elif check_cooldown(user_ip):
+            # If the user is on cooldown, return an error
             return jsonify({'status': 'error', 'message': 'Cooldown period not over.'})
+        
+        else:
+            # Add the user to the blacklist and set a cooldown
+            add_to_blacklist(user_ip)
+            return jsonify({'status': 'error', 'message': 'You are blacklisted.'})
 
-        url = request.form.get('url')
-        views = int(request.form.get('views'))
-        use_delay = 'use_delay' in request.form
-        delay = int(request.form.get('delay')) if use_delay else 0
-
-        # Perform your botting logic here...
-        # For example, you can call the simulate_views function to simulate views.
-        # Replace this with your actual botting logic.
-        print(f"Botting URL: {url} with {views} views.")
-        print(f"Use Delay: {use_delay}, Delay: {delay}")
-
-        success_count = simulate_views(url, views, use_delay, delay)
-
-        return jsonify({'status': 'success', 'views': success_count})
     else:
         return render_template('homepage.html')
-
 
 @app.route('/ebay.html')
 def ebay():
     return render_template('ebay.html')
-
 
 @app.route('/Us.html')
 def Us():
@@ -136,4 +178,4 @@ def tiktok():
     return render_template('tiktok.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=1337)
+    app.run(debug=False, host='0.0.0.0', port=1337)
